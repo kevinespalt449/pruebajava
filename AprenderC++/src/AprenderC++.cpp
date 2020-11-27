50f7697e5b006c5ec65613d916454b9454bd5fd8
= <=>
2020-11-27 02:54:39 +0100

    se subiran estos archivos a repositorio github que ya tenemos creado.

---
+++ AprenderC++/src/AprenderC++.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AprenderC++.cpp
+// Author      : Kevin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Conociendo los diferentes entornos y lenguajes de programacion!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
